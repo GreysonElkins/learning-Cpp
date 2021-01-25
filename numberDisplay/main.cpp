4dcbc71d45fbae1d97c6043faa2987793fb62c76
Greyson Elkins <greysonelkins@gmail.com>
2021-01-25 13:43:31 -0700

    Initial commit

---
+++ numberDisplay/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+// using namespace std; // brings in the entire std namespace
+using std::cout;
+using std::cin;
+
+int main() {
+  int favorite_number;
+  cout << "What's your favorite number between 1 and 100? ";
+  cin >> favorite_number;
+  cout << favorite_number << " is my favorite also!" 
+    << endl;
+  return 0;
+}
